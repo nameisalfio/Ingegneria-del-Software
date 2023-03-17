ab8ec7f0c90ee811659ce1825a0613b0a9778362
nameisalfio <alfio.spoto@outlook.it>
2023-03-17 13:25:36 +0100

    Added Singleton

---
+++ Java/Singleton/Logs.java
@@ -0,0 +1,53 @@
+package Java.Singleton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Logs è un Singleton 
+
+public class Logs
+{
+    //Tutti gli attributi statici 
+
+    private static Logs uniqueIstance;    //unica istanza 
+    private List<String> list;
+
+    private Logs()  //viene eseguito quando viene creata l'unica istanza della classe
+    {
+        list = new ArrayList<>();
+    }
+
+    public static Logs getIstance()
+    {
+        if(null == uniqueIstance)
+        {
+            uniqueIstance = new Logs();
+            return uniqueIstance;
+        }
+        return null;
+    }
+
+    public void insert(String val)
+    {
+        list.add(val);
+    }
+
+    public String  getLast()
+    {
+        return list.get(list.size()-1);
+    }
+
+    public String getAll()  //enhanced for
+    {
+        String toReturn = "";
+        for(String str : list)
+            toReturn = toReturn.concat(str);
+        return toReturn;
+    }
+
+    public void empty()
+    {
+        list = new ArrayList<>();
+    }
+
+}
---
+++ Java/Singleton/Main.java
@@ -0,0 +1,26 @@
+package Java.Singleton;
+
+//Main
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Logs logs = Logs.getIstance();
+
+        // Aggiunge alcuni dati al registro
+        logs.insert("Primo messaggio di log");
+        logs.insert("Secondo messaggio di log");
+        logs.insert("Terzo messaggio di log");
+
+        // Stampa l'ultimo messaggio di log
+        System.out.println("\nUltimo messaggio di log: " + logs.getLast());
+
+        // Stampa tutti i messaggi di log
+        System.out.println("Tutti i messaggi di log:");
+        System.out.println(logs.getAll());
+
+        // Svuota la lista dei messaggi di log
+        logs.empty();
+    }
+}
+
